312fb7b49eda20fe7f152753752c12d6e128a764
davidaf3 <david.alvarez.fidalgo@gmail.com>
2021-07-12 19:04:00 +0200

    Added match info

--- webapp/src/components/match/MatchDetails.vue
+++ webapp/src/components/match/MatchDetails.vue
@@ -1,12 +1,53 @@
 <template>
-  <div id="rounds">
-    <MoneyChart :rounds="rounds" />
-    <RoundsChart
-      :rounds="rounds"
-      :max-kills-per-round="match.mode === 'casual' ? 10 : 5"
-      :has-half-time="match.mode === 'competitive'"
-      :half-time-round="match.mode === 'competitive' ? 15 : null"
-    />
+  <div id="details">
+    <h1>{{ match.roundsWon }} - {{ match.roundsLost }}</h1>
+    <div id="matchInfo">
+      <img
+        :src="`img/map-icons/map_icon_${match.map}.svg`"
+        :alt="match.map"
+        height="50"
+      />
+      <div class="matchInfoBox">
+        <h5>Map</h5>
+        <span>{{ match.map }}</span>
+      </div>
+      <div class="matchInfoBox">
+        <h5>Mode</h5>
+        <span>{{
+          match.mode.charAt(0).toUpperCase() + match.mode.slice(1)
+        }}</span>
+      </div>
+      <div class="matchInfoBox">
+        <h5>Date</h5>
+        <span>
+          {{
+            matchDate.toLocaleDateString('en', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })
+          }},
+          {{
+            matchDate
+              .toLocaleTimeString()
+              .slice(0, matchDate.toLocaleTimeString().lastIndexOf(':'))
+          }}
+        </span>
+      </div>
+      <div class="matchInfoBox">
+        <h5>Duration</h5>
+        <span>{{ Math.round(match.duration / 60) }} min</span>
+      </div>
+    </div>
+    <div id="rounds">
+      <MoneyChart :rounds="rounds" />
+      <RoundsChart
+        :rounds="rounds"
+        :max-kills-per-round="match.mode === 'casual' ? 10 : 5"
+        :has-half-time="match.mode === 'competitive'"
+        :half-time-round="match.mode === 'competitive' ? 15 : null"
+      />
+    </div>
   </div>
 </template>
 
@@ -30,11 +71,13 @@
     data() {
       return {
         rounds: [],
+        matchDate: new Date(this.match.date),
       };
     },
     watch: {
       match() {
         this.updateRounds();
+        this.matchDate = new Date(this.match.date);
       },
     },
     created() {
@@ -44,7 +87,6 @@
       updateRounds() {
         getRounds(this.match.id).then(rounds => {
           this.rounds = rounds;
-          this.maxKillsPerRound = this.match.mode === 'casual' ? 10 : 5;
         });
       },
     },
@@ -52,10 +94,25 @@
 </script>
 
 <style>
+  h1 {
+    margin-bottom: 1em;
+  }
+  #details,
   #rounds {
     display: flex;
     flex-flow: column;
     justify-content: center;
     align-items: center;
+  }
+
+  #matchInfo {
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1em;
+  }
+  .matchInfoBox {
+    margin-left: 2em;
   }
 </style>
