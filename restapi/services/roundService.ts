1a4a44ad908e59261997593908241f75f31857a8
davidaf3 <david.alvarez.fidalgo@gmail.com>
2022-10-06 14:47:42 +0200

    Return only finished rounds

--- restapi/services/roundService.ts
+++ restapi/services/roundService.ts
@@ -69,19 +69,16 @@ export function saveCurrentRound(): Promise<void> {
 }
 
 /**
- * Finds all rounds of a match, ordered by round number
+ * Finds all finished rounds of a match, ordered by round number
  * @param matchId id of the match
  * @returns list of rounds of the match
  */
 export async function findByMatch(matchId: string): Promise<Rounds.Round[]> {
-  const rounds = await Rounds.getByMatch(matchId);
-  if (currentRound && matchId === currentRound.matchId)
-    rounds[currentRound.n - 1] = currentRound;
-  return rounds;
+  return Rounds.getByMatch(matchId);
 }
 
 /**
- * Finds a round of a match
+ * Finds a finished round of a match
  * @param matchId id of the match
  * @param n number of the round
  * @returns the round or null if there was no such round
@@ -90,11 +87,5 @@ export async function findByMatchAndNumber(
   matchId: string,
   n: string
 ): Promise<Rounds.Round | null> {
-  if (
-    currentRound &&
-    matchId === currentRound.matchId &&
-    Number(n) === currentRound.n
-  )
-    return currentRound;
-  return await Rounds.getByMatchAndNumber(matchId, n);
+  return Rounds.getByMatchAndNumber(matchId, n);
 }
