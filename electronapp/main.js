92f15d5560195fa5c25d8c7acce67e1377548ecd
Lucasion <lucasion@hotmail.com>
2025-05-16 19:28:04 +0200

    Add system tray functionality with context menu and icon

--- electronapp/main.js
+++ electronapp/main.js
@@ -1,5 +1,5 @@
 const {
-  app, protocol, BrowserWindow, shell, screen,
+  app, protocol, BrowserWindow, shell, screen, Tray, Menu, dialog
 } = require('electron');
 const path = require('path');
 const os = require('os');
@@ -10,7 +10,47 @@ const startRestAPI = require('csgo-tracker-restapi').default;
 const WIDTH = 1776;
 const HEIGHT = 1000;
 
+/**
+ * @type {BrowserWindow}
+ */
+let mainWindow;
+let tray = null;
+
 startRestAPI(path.join(os.homedir(), '.csgo-tracker', 'stats.db'));
+
+function createTray() {
+  const iconPath = path.join(__dirname, 'resources', 'trayIcon.png'); // Add an icon to your resources
+  tray = new Tray(iconPath);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open CSGO Tracker',
+      click: () => {
+        if (mainWindow) {
+          mainWindow.isVisible() ? mainWindow.focus() : mainWindow.show();
+        } else {
+          createWindow();
+        }
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.isQuitting = true;
+        app.quit();
+      }
+    }
+  ]);
+  tray.setToolTip('CSGO Tracker');
+  tray.setContextMenu(contextMenu);
+  
+  tray.on('click', () => {
+    if (mainWindow) {
+      mainWindow.isVisible() ? mainWindow.focus() : mainWindow.show();
+    } else {
+      createWindow();
+    }
+  });
+}
 
 function createWindow() {
   const FILE_PROTOCOL = 'file';
@@ -24,7 +64,7 @@ function createWindow() {
 
   const { screenWidth, screenHeight } = screen.getPrimaryDisplay().workArea;
 
-  const window = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     show: false,
     backgroundColor: '#282c34',
     width: WIDTH,
@@ -33,19 +73,53 @@ function createWindow() {
     y: screenHeight / 2 - HEIGHT / 2,
   });
 
-  window.webContents.setWindowOpenHandler((details) => {
+  mainWindow.webContents.setWindowOpenHandler((details) => {
     if (/^https:\/\//.test(details.url)) shell.openExternal(details.url);
     return { action: 'deny' };
   });
 
-  window.removeMenu();
-  window.loadURL('file:///index.html');
+  mainWindow.removeMenu();
+  mainWindow.loadURL('file:///index.html');
 
-  window.once('ready-to-show', () => {
-    window.show();
+  mainWindow.once('ready-to-show', () => {
+    mainWindow?.show();
+  });
+
+  mainWindow.on('close', (event) => {
+    if (!app.isQuitting) {
+      event.preventDefault();
+      
+      dialog.showMessageBox(mainWindow, {
+        type: 'question',
+        buttons: ['Run in the background', 'Quit'],
+        defaultId: 0,
+        title: 'Close CSGO Tracker',
+        message: 'Would you like to run the app in the background (Minimized to tray) or quit the application?'
+      }).then(({ response }) => {
+        if (response === 0) {
+          mainWindow.destroy();
+          mainWindow = null;
+        } else {
+          app.isQuitting = true;
+          app.quit();
+        }
+      });
+    }
   });
 }
 
 app.whenReady().then(() => {
   createWindow();
+  createTray();
+  
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (!app.isQuitting) return;
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
